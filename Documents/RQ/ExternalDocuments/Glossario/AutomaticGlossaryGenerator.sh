8109a506933026e1bddceece18631d848aa86b95
mzanella <zanna0150@gmail.com>
2016-04-27 10:42:03 +0200

    Revert "modifica script glossario"
    This reverts commit 1a4f1d2e84f2dded3d703e617fa5d2235d3fa576.

--- Documents/RQ/ExternalDocuments/Glossario/AutomaticGlossaryGenerator.sh
+++ Documents/RQ/ExternalDocuments/Glossario/AutomaticGlossaryGenerator.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-echo "This script require pdftk package becuse of a latex command that cannot be redefined..."
-echo "glossario.pl compiling:"
 perl glossario.pl
-echo "Latex compiling could take long time..."
-{
-	pdflatex Glossario.tex
-	makeglossaries Glossario
-	pdflatex Glossario
-	pdflatex Glossario
-	pdflatex Glossario
-	rm Glossario.aux
-	rm Glossario.glo
-	rm Glossario.glg
-	rm Glossario.gls
-	rm Glossario.ist
-	rm Glossario.log
-	rm Glossario.out
-	rm Glossario.toc
-} &> /dev/null
-pdftk Glossario.pdf cat 1-8 10-end output Glossario2.pdf
-rm Glossario.pdf
-mv Glossario2.pdf Glossario.pdf
-echo "Done!"
+pdflatex Glossario.tex
+makeglossaries Glossario
+pdflatex Glossario
+pdflatex Glossario
+rm Glossario.aux
+rm Glossario.glo
+rm Glossario.glg
+rm Glossario.gls
+rm Glossario.ist
+rm Glossario.log
+rm Glossario.out
--- Documents/RQ/ExternalDocuments/Glossario/glossario.pl
+++ Documents/RQ/ExternalDocuments/Glossario/glossario.pl
@@ -2,8 +2,7 @@
 use XML::LibXML;
 use strict;
 use utf8;
-#\\sectionfont{\\fontsize{0}{0}\\selectfont}
-#\\item\\makebox[\\linewidth]{\\Large\\textbf{\\glsgetgrouptitle{##1}}}%
+
 my $file = "Glossario.xml";
 my $parser=XML::LibXML->new();                      #creazione del parser
 my $doc=$parser->parse_file($file) || die("file non trovato \n");   #parsing del file
@@ -12,35 +11,19 @@ my @voci = $root->getElementsByTagName("voce");
 my $newfile = "\\documentclass[a4paper]{article}
 \\usepackage{leaf}
 \\usepackage[nonumberlist]{glossaries}
-\\usepackage{titletoc}
-\\usepackage{titlesec}
-
-
-
 \\titlepage{}
 \\newglossarystyle{myaltlistgroup}{%
 \\setcounter{page}{0}
   \\setglossarystyle{altlistgroup}%
   \\renewcommand*{\\glsgroupheading}[1]{%
    \\newpage
-    \\section{##1}%
+    \\item\\makebox[\\linewidth]{\\Large\\textbf{\\glsgetgrouptitle{##1}}}%
     \\vspace*{-\\baselineskip}%
-	
-    \\item\\makebox[\\linewidth]{\\hspace*{4cm}\\hrulefill\\hspace*{2cm}}%
-  	
+    \\item\\makebox[\\linewidth]{\\hspace*{3cm}\\hrulefill\\hspace*{3cm}}%
   }%
 }
-
-
-
-\\newcommand\\invisiblesection[1]{%
-  \\refstepcounter{subsection}%
-  \\addcontentsline{toc}{subsection}{\\protect\\numberline{\\thesection}#1}%
-  \\sectionmark{#1}
-}
-
 \\renewcommand{\\glossarysection}[2][]{}
-\\intestazioni{Glossario}";
+\\intestazioni{Glossario}\n";
 foreach my $voce (@voci) {
     my $termine = $voce->findvalue("termine");
     $termine =~ s/^\s+|\s+$//g;
@@ -48,15 +31,14 @@ foreach my $voce (@voci) {
     my $descrizione = $voce->findvalue("descrizione");
     $descrizione =~ s/^\s+|\s+$//g;
     $descrizione = ucfirst($descrizione);
-    $newfile = $newfile."\\newglossaryentry{$termine}{name=$termine,description={\\invisiblesection{$termine}$descrizione}}\n";
+    $newfile = $newfile."\\newglossaryentry{$termine}{name=$termine,description={$descrizione}}\n";
 }
 
 $newfile = $newfile."\\makeglossaries
 \\author{Zanella Marco}
 \\date{07/12/2015}
-
+\\intestazioni{Glossario}
 \\pagenumbering{gobble}
-\\setcounter{secnumdepth}{2}
 \\begin{document}
 \\begin{titlepage}
 	\\centering
@@ -81,30 +63,14 @@ $newfile = $newfile."\\makeglossaries
                                                     			& Prof. Miriade S.p.A.		\\\\[1ex]
 		\\end{tabular}
 	\\end{titlepage}
+	\\subfile{DiarioModificheG}
 
 	
-		
-		\\include{DiarioModificheG}
-		
-		\\newpage		
-		\\titlecontents{section}[1em]
-		{\\vskip 1ex}
-		{\\scshape\\bfseries\\fontsize{2em}{2em}\\selectfont}
-		{\\itshape}
-		{}
-		\\titlecontents{subsection}[2em]
-		{}
-		{}
-		{}
-		{\\titlerule\\contentspage}
-		\\tableofcontents
-	\\label{LastFrontPage}
-	\\pagebreak
 	\\pagestyle{mymain}
-	\\titleformat{\\section}[block]{\\bfseries\\filcenter\\fontsize{25pt}{25pt}\\selectfont}{}{1em}{}	
-	\\glsaddall		
+	\\glsaddall
 	\\printglossary[style=myaltlistgroup, nonumberlist]
-	\\label{LastPage}
+
+\\label{LastPage}
 \\end{document}";
 
 $file = "Glossario.tex";
